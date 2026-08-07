b7c86d59dd256c2127cade217848224ed90e6a6f
Satyaki <satyakichatterjee4@gmail.com>
2026-08-07 14:37:43 +0530

    pass

--- Board.py
+++ Board.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 from enum import Enum
+import sys
+import time
 
 
 class Board:
@@ -8,24 +10,12 @@ class Board:
         self.margin = "       "
         self.result = self.State.Pending
         self.glo = "\033[0m"
-        self.cell = [["0", "1", "2"], 
-                     ["3", "4", "5"], 
-                     ["6", "7", "8"]]
-        self.hlit = [["\033[38;2;100;100;100m","\033[38;2;100;100;100m","\033[38;2;100;100;100m"],
-                     ["\033[38;2;100;100;100m","\033[38;2;100;100;100m","\033[38;2;100;100;100m"],
-                     ["\033[38;2;100;100;100m","\033[38;2;100;100;100m","\033[38;2;100;100;100m"]]
-        self.available_moves = {(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)}
-        self.board = (
-            f"{self.margin}  {self.hlit[0][0]}{self.cell[0][0]}{self.glo}  |  {self.hlit[0][1]}{self.cell[0][1]}{self.glo}  |  {self.hlit[0][2]}{self.cell[0][2]}{self.glo}  \n"
-            f"{self.margin}-----------------\n"
-            f"{self.margin}  {self.hlit[1][0]}{self.cell[1][0]}{self.glo}  |  {self.hlit[1][1]}{self.cell[1][1]}{self.glo}  |  {self.hlit[1][2]}{self.cell[1][2]}{self.glo}  \n"
-            f"{self.margin}-----------------\n"
-            f"{self.margin}  {self.hlit[2][0]}{self.cell[2][0]}{self.glo}  |  {self.hlit[2][1]}{self.cell[2][1]}{self.glo}  |  {self.hlit[2][2]}{self.cell[2][2]}{self.glo}  \n"
-        )
+        self.red = "\033[91m"
+        self.blue = "\033[94m"
         self.usersign = user_sign
         self.aisign = chr(79 + 88 - ord(user_sign))
         self.refresh_terminal()
-        self.reset()
+        self.reset_board()
 
     @staticmethod
     def coordinates(pos: int)->tuple:
@@ -40,19 +30,56 @@ class Board:
             f"{self.margin}  {self.hlit[2][0]}{self.cell[2][0]}{self.glo}  |  {self.hlit[2][1]}{self.cell[2][1]}{self.glo}  |  {self.hlit[2][2]}{self.cell[2][2]}{self.glo}  \n"
         )
 
+    def slow_print(self, text, delay=0.0013):
+        for char in text:
+            sys.stdout.write(char)
+            sys.stdout.flush() 
+            time.sleep(delay)   
+        print() 
+
+    def announcer(self):
+        announce = (f"{self.ann_hol[0]}---------------------------------\n"
+                    f"        {self.ann_hol[1]}        "
+                    f"\n---------------------------------{self.glo}\n")
+        glow_sequence = ["\033[2;37m","\033[0;37m", "\033[1;37m", "\033[0;37m"]
+        for _ in range(5):
+            for style in glow_sequence:
+                sys.stdout.write(f"\r{style}{announce}\033[0m")
+                sys.stdout.flush()
+                time.sleep(0.15) 
+                sys.stdout.write('\033[3F')
+        sys.stdout.write('\033[3E')
+        print()
+
     def printer(self):
         self.refresh_terminal()
         self.set()
-        print(self.board)
+        self.slow_print(self.board)
         if(self.result == self.State.Draw):
-            print("It is a Draw.")
+            self.ann_hol[1] = "IT IS A DRAW."
+            self.announcer()
         elif(self.result == self.State.AI):
-            print("Alas! You Lost.")
+            self.ann_hol[0] = self.red
+            self.ann_hol[1] = "ALAS! YOU LOST."
+            self.announcer()
         elif(self.result == self.State.User):
-            print("Hurray! You Win.")
-    def reset(self):
+            self.ann_hol[0] = self.blue
+            self.ann_hol[1] = "HURRAY! YOU WIN."
+            self.announcer()
+
+
+    def reset_board(self):
         self.refresh_terminal()
         self.result = self.State.Pending
+        self.ann_hol = ["",""]
+        self.cell = [["0", "1", "2"], 
+                    ["3", "4", "5"], 
+                    ["6", "7", "8"]]
+        self.hlit = [["\033[38;2;100;100;100m","\033[38;2;100;100;100m","\033[38;2;100;100;100m"],
+                    ["\033[38;2;100;100;100m","\033[38;2;100;100;100m","\033[38;2;100;100;100m"],
+                    ["\033[38;2;100;100;100m","\033[38;2;100;100;100m","\033[38;2;100;100;100m"]]
+        self.available_moves = {(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)}
+        self.set()
         self.printer()
 
 
@@ -124,5 +151,3 @@ class Board:
         User = 1 
         Draw = 2
         Pending = 3
-
-
